d8b834a34d0ba9fdd69bd714e907415e04a08217
Nathaniel Chang <Changipro.nathaniel@gmail.com>
2022-02-20 15:40:52 +1030

    updating node server
    Updating the node server to display lyrics in the console.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+const io = require('socket.io');
+
 const WebSocket = require('ws');
 
 const app = express()
@@ -18,6 +20,40 @@ const socket = new WebSocket("ws://10.0.5.104:4444/remote");
 socket.onopen = () => {
 };
 
-socket.onmessage = (data) => {
-  console.log(data);
+var slide_number = 0;
+
+socket.onmessage = function (event) {
+    var msg = JSON.parse(event.data);
+    // console.log(msg);
+    
+    switch (msg.action) {
+        case "presentationCurrent":
+            console.log(slide_number);
+            x = msg.presentation.presentationSlideGroups[0].groupSlides[slide_number].slideText;
+            // document.getElementById("slide_text").innerHTML = x;
+            console.log(x);
+            break;
+        case "presentationTriggerIndex":
+            slide_number = msg.slideIndex;
+            // console.log(slide_number);
+            get_slide();
+            break;
+    }
 };
+
+// Check if the webscocket is connected
+function check_socket() {
+    if (!socket) return err("SOCKET NOT CONNECTED");
+    if (socket.readyState != 1) return err("SOCKET NOT READY");
+    return true;
+}
+
+function emit(obj) {
+    var json = JSON.stringify(obj);
+    if (check_socket()) socket.send(json);
+    else return err("SOCKET EMIT FAILED");
+}
+
+function get_slide() {
+    emit({ action: "presentationCurrent" });
+}
