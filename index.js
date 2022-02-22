d93ad352b57f67769153b2f1a144848fadd372bd
Nathaniel Chang <Changipro.nathaniel@gmail.com>
2022-02-22 16:54:43 +1030

    Connecting Client
    Connecting Client and server with wbs socket

--- index.js
+++ index.js
@@ -1,75 +1,153 @@
-var express = require('express');
-const io = require('socket.io');
-const { Client, Message } = require('node-osc');
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const defaultListenPort = 80;
+
+const fs = require("fs");
+
+const JSONdatafile = "presentationbridge-data.json";
+
+// vaiables for ProPresenter, resolume ip and port.
+var proPresenter_ip;
+var proPresenter_port;
+var resolume_ip;
+var resolume_port;
+
+
+// const { Client, Message } = require('node-osc');
 
 const WebSocket = require('ws');
 
-const app = express()
-const port = 3000
+//contecting to client (webview)
+io.sockets.on("connection", function(socket) {
+
+    socket.on("config_test", function(test_info) {
+        console.log(test_info);
+    })
+
+});
+
 
 // setting up osc
-const client = new Client('192.168.86.25', 7000);
+// const client = new Client('192.168.86.25', 7000);
 
-app.get('/', (req, res) => {
-    res.statusCode = 200;
+app.get('/', function (req, res) {
     res.sendFile(__dirname + '/views/index.html');
-})
+});
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+let listenPort = defaultListenPort;
 
-const socket = new WebSocket("ws://192.168.86.25:4444/remote");
+let cli_listenPort = process.argv[2];
 
-socket.onopen = () => {
-};
-
-var slide_number = 0;
-
-socket.onmessage = function (event) {
-    var msg = JSON.parse(event.data);
-    // console.log(msg);
+if (parseInt(cli_listenPort) !== 'NaN')
+{
+    intPort = parseInt(cli_listenPort);
     
-    switch (msg.action) {
-        case "presentationCurrent":
-            console.log(slide_number);
-            x = msg.presentation.presentationSlideGroups[0].groupSlides[slide_number].slideText;
-            // document.getElementById("slide_text").innerHTML = x;
-
-            // assigning what the message is to resolume
-            var message = new Message('/composition/layers/4/clips/1/video/source/textgenerator/text/params/lines');
-            message.append(x);
-
-            // sending the message to resolume
-            client.send(message, (err) => {
-                if (err) {
-                    console.error(new Error(err));
-                }
-            });
-
-            console.log(x);
-            break;
-        case "presentationTriggerIndex":
-            slide_number = msg.slideIndex;
-            // console.log(slide_number);
-            get_slide();
-            break;
+    if ((intPort > 1024) && (intPort <= 65535))
+    {
+        listenPort = intPort;
     }
-};
-
-// Check if the webscocket is connected
-function check_socket() {
-    if (!socket) return err("SOCKET NOT CONNECTED");
-    if (socket.readyState != 1) return err("SOCKET NOT READY");
-    return true;
 }
 
-function emit(obj) {
-    var json = JSON.stringify(obj);
-    if (check_socket()) socket.send(json);
-    else return err("SOCKET EMIT FAILED");
+http.listen(listenPort, function () {
+    console.log("listening on *:" + listenPort);
+});
+
+
+
+function loadFile() //loads settings on first load of app
+{   
+    try
+    {
+        let rawdata = fs.readFileSync(JSONdatafile); 
+        let myJson = JSON.parse(rawdata); 
+
+        proPresenter_ip = myJson.proPresenter_ip;
+        proPresenter_port = myJson.proPresenter_port;
+        resolume_ip = myJson.resolume_ip;
+        resolume_port = myJson.resolume_port;
+    }
+    catch (error)
+    {
+        
+    }
 }
 
-function get_slide() {
-    emit({ action: "presentationCurrent" });
+function saveFile() //saves settings to a local storage file for later recalling
+{
+    var myJson = {
+        ConfigPassword: ConfigPassword,
+        Bridges: Bridges
+    };
+
+    fs.writeFileSync(JSONdatafile, JSON.stringify(myJson), "utf8", function(error) {
+        if (error)
+        { 
+          console.log('error: ' + error);
+        }
+        else
+        {
+          console.log('file saved');
+        }
+    });
 }
+
+
+
+// LISTIENING FOR PROPRESENTER
+
+// const socket = new WebSocket("ws://192.168.86.25:4444/remote");
+
+// socket.onopen = () => {
+// };
+
+// var slide_number = 0;
+
+// socket.onmessage = function (event) {
+//     var msg = JSON.parse(event.data);
+//     // console.log(msg);
+    
+//     switch (msg.action) {
+//         case "presentationCurrent":
+//             console.log(slide_number);
+//             x = msg.presentation.presentationSlideGroups[0].groupSlides[slide_number].slideText;
+//             // document.getElementById("slide_text").innerHTML = x;
+
+//             // assigning what the message is to resolume
+//             var message = new Message('/composition/layers/4/clips/1/video/source/textgenerator/text/params/lines');
+//             message.append(x);
+
+//             // sending the message to resolume
+//             client.send(message, (err) => {
+//                 if (err) {
+//                     console.error(new Error(err));
+//                 }
+//             });
+
+//             console.log(x);
+//             break;
+//         case "presentationTriggerIndex":
+//             slide_number = msg.slideIndex;
+//             // console.log(slide_number);
+//             get_slide();
+//             break;
+//     }
+// };
+
+// // Check if the webscocket is connected
+// function check_socket() {
+//     if (!socket) return err("SOCKET NOT CONNECTED");
+//     if (socket.readyState != 1) return err("SOCKET NOT READY");
+//     return true;
+// }
+
+// function emit(obj) {
+//     var json = JSON.stringify(obj);
+//     if (check_socket()) socket.send(json);
+//     else return err("SOCKET EMIT FAILED");
+// }
+
+// function get_slide() {
+//     emit({ action: "presentationCurrent" });
+// }
