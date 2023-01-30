0ec5767493ff809c75dd0068f8e8b7c2b303f2ef
Nathaniel Chang <changipro.nathaniel@gmail.com>
2023-01-30 13:17:50 +1030

    Cleaning up the code

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 const defaultListenPort = 8000;
 
 const fs = require("fs");
@@ -9,34 +9,28 @@ const fs = require("fs");
 const JSONdatafile = "presentationbridge-data.json";
 
 // onbject for ProPresenter, resolume ip and port.
-let ip_information_local = {}
+let ip_information_local = {};
 
+const { Client, Message } = require("node-osc");
 
-const { Client, Message } = require('node-osc');
-
-const WebSocket = require('ws');
+const WebSocket = require("ws");
 
 //contecting to client (webview)
-io.sockets.on("connection", function(socket) {
-
-    socket.on("ip_update", function(ip_information) {
-
+io.sockets.on("connection", function (socket) {
+    socket.on("ip_update", function (ip_information) {
         ip_information_local = ip_information;
         saveFile();
-        
-    })
-
-    socket.on("load_request", function() {
-        io.emit("data", ip_information_local);
-    })
-
-    // request for connecting the nodes
-    socket.on("connect_nodes", function() {
-        connect_nodes();
     });
 
-});
+    socket.on("load_request", function () {
+        io.emit("data", ip_information_local);
+    });
 
+    // request for connecting the nodes
+    socket.on("connect_nodes", function () {
+        connect_nodes();
+    });
+});
 
 // setting up connection variables
 var client = null;
@@ -44,15 +38,23 @@ var socket = null;
 
 // opening sockets for connecting to nodes
 function connect_nodes() {
-    client = new Client(ip_information_local.resolumeIP, ip_information_local.resolumePORT);
-    socket = new WebSocket("ws://"+ ip_information_local.proPresenterIP +":" + ip_information_local.proPresenterPORT + "/remote");
+    client = new Client(
+        ip_information_local.resolumeIP,
+        ip_information_local.resolumePORT
+    );
+    socket = new WebSocket(
+        "ws://" +
+            ip_information_local.proPresenterIP +
+            ":" +
+            ip_information_local.proPresenterPORT +
+            "/remote"
+    );
 
-    socket.onopen = () => {
-    };
+    socket.onopen = () => {};
 
     var slide_number = 0;
 
-    lyrics_received()
+    lyrics_received();
 }
 
 function getItem(presentationSlideGroups, targetItem) {
@@ -60,41 +62,49 @@ function getItem(presentationSlideGroups, targetItem) {
     for (let group = 0; group < presentationSlideGroups.length; group++) {
         const groupSlides = presentationSlideGroups[group].groupSlides;
         for (let slide = 0; slide < groupSlides.length; slide++) {
-            itemCount++;
             if (itemCount === targetItem) return groupSlides[slide];
+            itemCount++;
         }
     }
 }
 
-
-function lyrics_received(){
+function lyrics_received() {
     socket.onmessage = function (event) {
         var msg = JSON.parse(event.data);
-        
+
         switch (msg.action) {
             case "presentationCurrent":
+                var number_of_groups =
+                    msg.presentation.presentationSlideGroups.length;
 
-                var number_of_groups = msg.presentation.presentationSlideGroups.length;
-
-                var item = getItem(msg.presentation.presentationSlideGroups, slide_number);
-                console.log(item.slideText);
+                var item = getItem(
+                    msg.presentation.presentationSlideGroups,
+                    slide_number
+                );
 
                 //assigning what the message is to resolume
-                var message = new Message('/composition/layers/4/clips/1/video/source/textgenerator/text/params/lines');
-                message.append(item.slideText);
-    
-                // sending the message to resolume
-                try{
+                var message = new Message(
+                    "/composition/layers/4/clips/2/video/source/textgenerator/text/params/lines"
+                );
+                if (item) {
                     message.append(item.slideText);
+                } else {
+                    console.log("Item not found");
+                }
+
+                // sending the message to resolume
+                try {
+                    if (item) {
+                        message.append(item.slideText);
+                    } else {
+                        console.log("Item not found");
+                    }
                     client.send(message, (err) => {
                         if (err) {
                             console.error(new Error(err));
                         }
                     });
-                }
-                catch(err){
-
-                }
+                } catch (err) {}
 
                 break;
             case "presentationTriggerIndex":
@@ -106,59 +116,51 @@ function lyrics_received(){
     };
 }
 
+app.get("/config", function (req, res) {
+    res.sendFile(__dirname + "/views/index.html");
+});
 
-app.get('/config', function (req, res) {
-    res.sendFile(__dirname + '/views/index.html');
-})
-
-
-function loadFile() //loads settings on first load of app
-{   
-    try
-    {
-        let rawdata = fs.readFileSync(JSONdatafile); 
-        let myJson = JSON.parse(rawdata); 
+function loadFile() {
+    //loads settings on first load of app
+    try {
+        let rawdata = fs.readFileSync(JSONdatafile);
+        let myJson = JSON.parse(rawdata);
 
         ip_information_local = myJson.ip_information;
-    }
-    catch (error)
-    {
-        
-    }
+    } catch (error) {}
 }
 
-function saveFile() //saves settings to a local storage file for later recalling
-{
+function saveFile() {
+    //saves settings to a local storage file for later recalling
     var myJson = {
-        ip_information: ip_information_local
+        ip_information: ip_information_local,
     };
 
     console.log(myJson);
 
-    fs.writeFileSync(JSONdatafile, JSON.stringify(myJson), "utf8", function(error) {
-        if (error)
-        { 
-          console.log('error: ' + error);
+    fs.writeFileSync(
+        JSONdatafile,
+        JSON.stringify(myJson),
+        "utf8",
+        function (error) {
+            if (error) {
+                console.log("error: " + error);
+            } else {
+                console.log("file saved");
+            }
         }
-        else
-        {
-          console.log('file saved');
-        }
-    });
+    );
 }
-
 
 // Listening to for the client
 let listenPort = defaultListenPort;
 
 let cli_listenPort = process.argv[2];
 
-if (parseInt(cli_listenPort) !== 'NaN')
-{
+if (parseInt(cli_listenPort) !== "NaN") {
     intPort = parseInt(cli_listenPort);
-    
-    if ((intPort > 1024) && (intPort <= 65535))
-    {
+
+    if (intPort > 1024 && intPort <= 65535) {
         listenPort = intPort;
     }
 }
@@ -166,8 +168,6 @@ if (parseInt(cli_listenPort) !== 'NaN')
 http.listen(listenPort, function () {
     console.log("listening on *:" + listenPort);
 });
-
-
 
 // LISTIENING FOR PROPRESENTER
 
@@ -181,7 +181,7 @@ http.listen(listenPort, function () {
 // socket.onmessage = function (event) {
 //     var msg = JSON.parse(event.data);
 //     // console.log(msg);
-    
+
 //     switch (msg.action) {
 //         case "presentationCurrent":
 //             console.log(slide_number);
